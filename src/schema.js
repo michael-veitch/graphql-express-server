bdc79971a0de4620575e934260d10334c71114f9
Mike <mikeveitch09876@gmail.com>
2018-04-06 00:01:50 +0200

    Implemented JSON Server /w Axios to mock out HTTP calls

--- src/schema.js
+++ src/schema.js
@@ -1,3 +1,5 @@
+//Axios is a client for making Http 
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,30 +9,34 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-const customers = [
-    {
-        id: '1',
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        age: 35
-    },
-    {
-        id: '2',
-        name: 'Jane Smith',
-        email: 'jsmith@gmail.com',
-        age: 35
-    },{
-        id: '3',
-        name: 'Bob Flannigan',
-        email: 'bflan@gmail.com',
-        age: 35
-    },{
-        id: '4',
-        name: 'Chantel El\'hefe',
-        email: 'boss@gmail.com',
-        age: 35
-    },
-]
+//TODO: Move to MongoDB
+// const customers = [
+//     {
+//         id: 1,
+//         name: "Leanne Graham",
+//         username: "Bret",
+//         email: "Sincere@april.biz",
+//         address: {
+//           street: "Kulas Light",
+//           suite: "Apt. 556",
+//           city: "Gwenborough",
+//           zipcode: "92998-3874",
+//           geo: {
+//             lat: "-37.3159",
+//             lng: "81.1496"
+//           }
+//         },
+//         phone: "1-770-736-8031 x56442",
+//         website: "hildegard.org",
+//         company: {
+//           name: "Romaguera-Crona",
+//           catchPhrase: "Multi-layered client-server neural-net",
+//           bs: "harness real-time e-markets"
+//         }
+//       }
+// ]
+
+
 
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
@@ -38,27 +44,61 @@ const CustomerType = new GraphQLObjectType({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         email: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        address: { type: AddressType },
+         phone: { type: GraphQLString},
+         website: { type: GraphQLString},
+         company: { type: CompanyType }
     })
 })
+const AddressType = new GraphQLObjectType({
+    name: 'Address',
+    fields: () => ({
+        street: { type: GraphQLString},
+        suite: { type: GraphQLString},
+        city: { type: GraphQLString},
+        zipcode: { type: GraphQLString},
+        geo: { type : GeoType }
+    })
+})
+const GeoType = new GraphQLObjectType({
+    name: 'Geo',
+    fields: () => ({
+        lat: { type: GraphQLString},
+        long: { type: GraphQLString}
+    })
+})
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: () => ({
+        name: { type: GraphQLString},
+        catchPhrase: { type: GraphQLString},
+        bs: { type: GraphQLString}
+    })
+})
+
+
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        //Single Customer by ID
         customer: {
             type: CustomerType,
             args: {
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args){
-                for(let i = 0; i < customers.length; customers++){
-                    if(customers[i].id == args.id){
-                        return customers[i]
-                    }
-                }
+                // for(let i = 0; i < customers.length; customers++){
+                //     if(customers[i].id == args.id){
+                //         return customers[i]
+                //     }
+                // }
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data)
             }
         },
+        //All Customers
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
--- src/server.js
+++ src/server.js
@@ -10,6 +10,6 @@ app.use('/graphql', expressGraphQL({
 }))
 
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(4200, () => {
+    console.log('Example app listening on port 4200!');
 });   
